a4372cc3d74d5078333939df2926f30daaf5d3c2
EragonJ <jack.xxlong@gmail.com>
2010-11-14 21:02:09 +0800

    Fix all

--- demo.php
+++ demo.php
@@ -1,7 +1,13 @@
 <?php
   include_once("RssParser.php");
 
-  $plurkUsers = array("http://www.plurk.com/EragonJ.xml","http://www.plurk.com/buweichiu.xml","http://www.plurk.com/BlackACE.xml");
+  $plurkUsers = array(
+      "http://www.plurk.com/EragonJ.xml",
+      "http://www.plurk.com/buweichiu.xml",
+      "http://www.plurk.com/BlackACE.xml",
+      "http://www.plurk.com/SS33.xml"
+  );
+
   $rss = new RssParser($plurkUsers);
   print_r($rss->exec()->sortSeeds()->toHTML(10));
 ?>
