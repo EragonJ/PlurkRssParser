53dd3c45d2c52426bfe6014b1b17491411e7c2f1
EragonJ <jack.xxlong@gmail.com>
2010-11-14 20:33:31 +0800

    Fix problems in html and add some js effect

--- RssParser.php
+++ RssParser.php
@@ -15,6 +15,7 @@
 
     public function __construct($filename)
     {
+      date_default_timezone_set("Asia/Taipei");
       $this->feedList = is_array($filename) ? $filename : array($filename);
       return $this;
     }
@@ -30,10 +31,15 @@
           $innerNodes = $DOMNode->childNodes;
           if(!empty($innerNodes))
           {
-            $title = $innerNodes->item(1)->nodeValue;
+            $allContent = $innerNodes->item(13)->nodeValue;
+            preg_match("/(.*?) (.*)/",$allContent,$allContent);
+
             $published = $innerNodes->item(7)->nodeValue;
             $link = $innerNodes->item(9)->attributes->item(0)->nodeValue;
-            $this->feedValue[] = array($title,$published,$link);
+            $nick = $allContent[1];
+            $content = $allContent[2];
+
+            $this->feedValue[] = array($nick,$content,$published,$link);
           }
         }
       }
@@ -45,26 +51,36 @@
 
       function cmp($a,$b)
       {
-        if($a[1] == $b[1])
+        if($a[2] == $b[2])
         {
           return 0;
         }
-        return ($a[1] < $b[1]) ? 1 : -1;
+        return ($a[2] < $b[2]) ? 1 : -1;
       }
 
       usort($this->feedValue,"cmp");
       return $this;
     }
 
-    public function toHTML()
+    public function toHTML($length)
     {
-      $this->html = "<html><head><meta http-equiv='content-type' content='text/html;charset=UTF-8'/><title>Plurk Widget</title><style type='text/css'> body { background-color: #cf682f; color: #f5e0d5 !important; } .user_info { border-bottom: 1px solid #e3a481; } .plurk-timeline { background-color: #cae7fd; border-bottom: 1px solid #e3a481; } a { color: #ffffff; } .plurk-plurks, .plurk-plurks a { color: #111111 !important; } .plurk-message { border-bottom: 1px solid #a8d7fc; } .plurk_on { background-color: #e3f2fe; } .plurk-meta, .plurk-meta a { color: #098df2 !important; } .action { background-color: #9d4f24; }</style><link rel='stylesheet' type='text/css' href='http://statics.plurk.com/5dfb4e9b0ae2b0e05ec2e92070d593c7.css.cgz?'></link><body><div class='container'><div id='content'><div id='plurks_holder'><div class='plurk-timeline plurk-plurks' style='height:318px;'>";
+      $i = 0;
+
+      $this->html = "<html><head><meta http-equiv='content-type' content='text/html;charset=UTF-8'/><title>Plurk Widget</title><style type='text/css'> body { background-color: #cf682f; color: #f5e0d5 !important; } .user_info { border-bottom: 1px solid #e3a481; } .plurk-timeline { background-color: #cae7fd; border-bottom: 1px solid #e3a481; } a { color: #ffffff; } .plurk-plurks, .plurk-plurks a { color: #111111 !important; } .plurk-message { border-bottom: 1px solid #a8d7fc; } .plurk_on { background-color: #e3f2fe; } .plurk-meta, .plurk-meta a { color: #098df2 !important; } .action { background-color: #9d4f24; }</style><link rel='stylesheet' type='text/css' href='http://statics.plurk.com/5dfb4e9b0ae2b0e05ec2e92070d593c7.css.cgz?'></link><script src='http://ajax.googleapis.com/ajax/libs/jquery/1.4.3/jquery.min.js'></script><script>$(document).ready(function(){\$('.plurk-message').mouseenter(function(){\$(this).toggleClass('plurk_on');}).mouseleave(function(){\$(this).toggleClass('plurk_on');})});</script></head><body><div class='container'><div id='content'><div id='plurks_holder'><div class='plurk-timeline plurk-plurks' style='height:318px;'>";
 
       foreach($this->feedValue as $userData)
       {
-         $this->html .= "<div class='plurk-message'>".$userData[0];
-         $this->html .= "<div class='plurk-meta'>".date("Y/m/d h:i:s",strtotime($userData[1]))." | <a href='http://www.plurk.com".$userData[2]."' target='_blank'>Read More</a></div>";
-         $this->html .= "</div>";
+        $this->html .= "<div class='plurk-message'>";
+        $this->html .= "<span class='plurk-nickname'>".$userData[0]."</span>";
+        $this->html .= "<span class='cnt'> ".$userData[1]."</span>";
+        $this->html .= "<div class='plurk-meta'>".date("Y/m/d h:i:s",strtotime($userData[2]))." | <a href='http://www.plurk.com".$userData[3]."' target='_blank'>Read More</a></div>";
+        $this->html .= "</div>";
+
+        $i++;
+        if($i >= $length)
+        { 
+          break;
+        }
       }
 
       $this->html .= "</div></div></div></body></html>";
--- demo.php
+++ demo.php
@@ -3,5 +3,5 @@
 
   $plurkUsers = array("http://www.plurk.com/EragonJ.xml","http://www.plurk.com/buweichiu.xml","http://www.plurk.com/BlackACE.xml");
   $rss = new RssParser($plurkUsers);
-  print_r($rss->exec()->sortSeeds()->toHTML());
+  print_r($rss->exec()->sortSeeds()->toHTML(10));
 ?>
